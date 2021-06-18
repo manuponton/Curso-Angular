32b3dad0dda12243bffdb28adbb7a69ac36f7e4e
manuponton <manuealejandro09@gmail.com>
2021-06-18 18:59:46 -0500

    Directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TitulosComponent } from './titulos/titulos.component';
+import { DirectivaComponent } from './directiva/directiva.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TitulosComponent ],
+  declarations: [ AppComponent, HelloComponent, TitulosComponent, DirectivaComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
